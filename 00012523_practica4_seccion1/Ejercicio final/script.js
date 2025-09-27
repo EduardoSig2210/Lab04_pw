50b5b0ab6774fa6b04d013e29c1b8439285465cf
EduardoSig2210 <0001252@uca.edu.sv>
2025-09-26 19:47:31 -0600

    Feat: completando ejercicio

--- 00012523_practica4_seccion1/Ejercicio final/script.js
+++ 00012523_practica4_seccion1/Ejercicio final/script.js
@@ -1,1 +1,23 @@
-Function 
+const actividades={
+    lunes: "atender a un cliente en especifico",
+    martes: "visitar a una agencia fuera de la ciudad",
+    miercoles: "llevar a mi hija al ballet",
+    jueves: "priorizar entregasr de desarrollo",
+    viernes: "atender problemas de manera remota",
+    sabado: "hacer lo que mi esposa quiera"
+}
+
+function recordatorio(dia){
+    dia=dia.toLowerCase();
+    if (actividades[dia]) {
+        return "Hoy es " + dia + "y debo " + actividades[dia];
+    } else {
+        return "No tienes actividades programadas para '" + dia + "'";
+    }
+}
+
+function mostrarRecordatorio(){
+    const diaIngresado = document.getElementById("dia").ariaValueMax.trim()
+    const mensaje = recordatorio(diaIngresado);
+    document.getElementById("evento").innerText = mensaje;
+}
