c18f18db5b7f8992abe8fe96833726689b18396e
EduardoSig2210 <0001252@uca.edu.sv>
2025-09-26 19:13:54 -0600

    Feat:  Completando y empezando ejercicio final

--- 00012523_practica4_seccion1/Ejercicio3/script.js
+++ 00012523_practica4_seccion1/Ejercicio3/script.js
@@ -12,4 +12,22 @@ function ordenar(arr){
     return nuevoArray;
 }
 
-console.log(ordenar([14, 23, 99, 874, 93, 12]))
+function esPar(numero){
+    return numero %2 === 0;
+}
+
+function calcularDiasCrecimiento(velocidadCrecimiento, VelocidadDecrecimiento, alturaDeseada){
+    let altura = 0;
+    let dias = 0;
+    while (altura < alturaDeseada) {
+        altura += velocidadCrecimiento;
+        dias++;
+        if(altura >= alturaDeseada) {return dias}
+        altura -= VelocidadDecrecimiento
+    }
+    return dias;
+}
+
+console.log(ordenar([14, 23, 99, 874, 93, 12]));
+console.log(esPar(7));
+console.log(calcularDiasCrecimiento(2,3,15));
